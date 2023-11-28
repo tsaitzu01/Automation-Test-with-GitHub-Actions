b4e846d80ca8d2cd3d49d22b14f8a112c197caca
Kathy Tsai <tsaitzu01@gmail.com>
2023-11-29 00:40:04 +0800

    Update entrypoint.sh

--- .github/actions/collect-logs/entrypoint.sh
+++ .github/actions/collect-logs/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-# Create a directory to store logs
 mkdir -p /github/workspace/logs
+cp -R /github/workspace/allure-results /github/workspace/logs
 
-# Copy the entire workspace to the logs directory excluding the logs directory
-find /github/workspace -mindepth 1 -maxdepth 1 -name logs -prune -o -exec cp -R '{}' /github/workspace/logs \;
-
-# Output logs-path value
 echo "::set-output name=logs-path::/github/workspace/logs"
