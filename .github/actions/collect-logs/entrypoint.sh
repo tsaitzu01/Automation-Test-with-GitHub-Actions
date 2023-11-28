ecd9900241134ca291aad69e90a2d1c1a4e7b029
Kathy Tsai <tsaitzu01@gmail.com>
2023-11-29 00:08:11 +0800

    Update entrypoint.sh

--- .github/actions/collect-logs/entrypoint.sh
+++ .github/actions/collect-logs/entrypoint.sh
@@ -4,4 +4,5 @@
 mkdir -p /github/workspace/logs
 
 # Copy the entire workspace to the logs directory excluding the logs directory
-rsync -a --exclude=logs /github/workspace/ /github/workspace/logs
+shopt -s extglob
+cp -R /github/workspace/!(logs) /github/workspace/logs
